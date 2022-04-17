b5ec9752524d4f8d1465e83863dc6dee9e337524
Gerd Reiss <gerd@reiss.pro>
2022-04-17 17:46:03 +0200

    [Apache Kafka for Beginners v3] remove response file

--- udemy/LearnApacheKafkaForBeginnersV3/kafka-consumer-wikimedia/using-zio-kafka/src/main/scala/WikimediaChangesProducer.scala
+++ udemy/LearnApacheKafkaForBeginnersV3/kafka-consumer-wikimedia/using-zio-kafka/src/main/scala/WikimediaChangesProducer.scala
@@ -1,22 +1,30 @@
-// import sttp.capabilities.zio.ZioStreams
-// import sttp.capabilities.zio.ZioStreams
-// import sttp.client3.httpclient.zio.*
-// import zio.stream.*
+import sttp.capabilities.zio.ZioStreams
+import sttp.client3.*
+import sttp.client3.httpclient.zio.HttpClientZioBackend
+import sttp.client3.httpclient.zio.SttpClient
+import sttp.client3.httpclient.zio.send
 import zio.*
+import zio.stream.*
+
+import scala.concurrent.duration.Duration
 
 object WikimediaChangesProducer extends ZIOAppDefault:
 
-  // FIXME: That stuff doesn't even compile:
+  // FIXME: That stuff doesn't even compile when including zio-kafka dependency:
   //  java.lang.RuntimeException: Conflicting cross-version suffixes in: org.scala-lang.modules:scala-collection-compat
 
-  // val request =
-  //   basicRequest
-  //     .get(uri"https://stream.wikimedia.org/v2/stream/recentchange")
-  //     .response(asStreamUnsafe(ZioStreams))
-  //     .readTimeout(java.time.Duration.Inf)
-  // val response = send(request)
-  //   .tapEach(r => println(r.body))
+  val request  =
+    basicRequest
+      .get(uri"https://stream.wikimedia.org/v2/stream/recentchange")
+      .response(asStreamUnsafe(ZioStreams))
+  val response = send(request)
+    .map(_.body)
+    .tap {
+      case Left(error)   =>
+        Console.printLine(s"Error: $error")
+      case Right(stream) =>
+        stream.map(println).runDrain
+    }
 
   override def run =
-    Console.printLine("Hello Kafka Clients")
-    // response.provide(HttpClientZioBackend.layer(), Console.live)
+    response.provide(HttpClientZioBackend.layer())
