60d346063bfcbf0a08da02d31501e42d64ed3420
Gerd Reiss <gerd@reiss.pro>
2022-04-17 20:05:47 +0200

    [Apache Kafka for Beginners v3] FS2 streaming works, ZIO doesn't compile

--- udemy/LearnApacheKafkaForBeginnersV3/kafka-consumer-wikimedia/using-fs2-kafka/src/main/scala/WikimediaChangesProducer.scala
+++ udemy/LearnApacheKafkaForBeginnersV3/kafka-consumer-wikimedia/using-fs2-kafka/src/main/scala/WikimediaChangesProducer.scala
@@ -1,36 +1,46 @@
-import cats.effect.ExitCode
-import cats.effect.IO
-import cats.effect.IOApp
-import fs2.Stream
-import fs2.io.stdout
-import fs2.text.lines
-import fs2.text.utf8
-import io.circe.Json
-import io.circe.jawn.CirceSupportParser
+import cats.implicits.*
+import cats.effect.*
+import fs2.*
+import fs2.kafka.*
 import org.http4s.Request
 import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.syntax.literals.uri
-import org.typelevel.jawn.Facade
-import org.typelevel.jawn.fs2.*
-import org.typelevel.jawn.AsyncParser
+
+import scala.jdk.CollectionConverters.*
+import cats.data.Ior
+import scala.util.Random
 
 object WikimediaChangesProducer extends IOApp:
 
-  given Facade[Json] = new CirceSupportParser(None, false).facade
+  // number of configured partitions for the topic
+  val partitions      = 3
+  // the group size for the records to be produced before sending them to Kafka
+  val recordGroupSize = 10
+  // the stream URL
+  val recentchangeUrl = uri"https://stream.wikimedia.org/v2/stream/recentchange"
+  // the topic name
+  val topic           = "wikipedia-recent-changes"
+  // the record key prefix
+  val keyprefix       = "fs2-producer-key-"
 
-  // FIXME: this one fails with:
-  //  org.typelevel.jawn.ParseException: expected json value got ':ok
+  val producerSettings = ProducerSettings[IO, String, String].withBootstrapServers("localhost:9092")
+
+  val recentChangesStream =
+    for
+      resource <- Stream.resource(EmberClientBuilder.default[IO].build)
+      stream   <- resource.stream(Request[IO](uri = recentchangeUrl))
+      records  <- stream.body.chunks
+                    .map(chunk => new String(chunk.toArray))
+                    .filter(_.startsWith("data: "))
+                    .map(_.drop(6))
+                    .map(ProducerRecord(topic, keyprefix + Random.nextInt(partitions), _))
+                    .sliding(recordGroupSize, recordGroupSize)
+                    .map(ProducerRecords.apply)
+    yield records
+
   def run(args: List[String]): IO[ExitCode] =
-    Stream
-      .resource(EmberClientBuilder.default[IO].build)
-      .flatMap {
-        _.stream(Request[IO](uri = uri"https://stream.wikimedia.org/v2/stream/recentchange"))
-          .flatMap(_.body.chunks.parseJsonStream)
-      }
-      .map(_.spaces2)
-      .through(lines)
-      .through(utf8.encode)
-      .through(stdout)
+    recentChangesStream
+      .through(KafkaProducer.pipe(producerSettings))
       .compile
       .drain
       .as(ExitCode.Success)
--- udemy/LearnApacheKafkaForBeginnersV3/kafka-consumer-wikimedia/using-zio-kafka/src/main/scala/WikimediaChangesProducer.scala
+++ udemy/LearnApacheKafkaForBeginnersV3/kafka-consumer-wikimedia/using-zio-kafka/src/main/scala/WikimediaChangesProducer.scala
@@ -1,29 +1,51 @@
 import sttp.capabilities.zio.ZioStreams
 import sttp.client3.*
-import sttp.client3.httpclient.zio.HttpClientZioBackend
-import sttp.client3.httpclient.zio.SttpClient
-import sttp.client3.httpclient.zio.send
+import sttp.client3.httpclient.zio.*
 import zio.*
 import zio.stream.*
+//import org.apache.kafka.clients.producer.{ KafkaProducer, ProducerRecord }
+// import zio.kafka.producer.*
+// import zio.kafka.serde.*
 
 import scala.concurrent.duration.Duration
+import scala.jdk.CollectionConverters.*
 
+// FIXME: That stuff doesn't even compile when including zio-kafka dependency:
+//  java.lang.RuntimeException: Conflicting cross-version suffixes in: org.scala-lang.modules:scala-collection-compat
 object WikimediaChangesProducer extends ZIOAppDefault:
 
-  // FIXME: That stuff doesn't even compile when including zio-kafka dependency:
-  //  java.lang.RuntimeException: Conflicting cross-version suffixes in: org.scala-lang.modules:scala-collection-compat
+  // number of configured partitions for the topic
+  val partitions      = 3
+  // the group size for the records to be produced before sending them to Kafka
+  val recordGroupSize = 10
+  // the stream URL
+  val recentchangeUrl = uri"https://stream.wikimedia.org/v2/stream/recentchange"
+  // the topic name
+  val topic           = "wikipedia-recent-changes"
+  // the record key prefix
+  val keyprefix       = "zio-producer-key-"
 
-  val request  =
+  val request =
     basicRequest
       .get(uri"https://stream.wikimedia.org/v2/stream/recentchange")
       .response(asStreamUnsafe(ZioStreams))
+
   val response = send(request)
     .map(_.body)
     .tap {
       case Left(error)   =>
         Console.printLine(s"Error: $error")
       case Right(stream) =>
-        stream.map(println).runDrain
+        stream.chunks
+          .map(chunk =>
+            new String(chunk.toArray).lines.toList.asScala
+              .find(_.startsWith("data: "))
+              .map(_.drop(6))
+            // .map(ProducerRecord(topic, keyprefix + Random.nextInt(partitions), _))
+          )
+          .take(1)
+          .map(println)
+          .runDrain
     }
 
   override def run =
